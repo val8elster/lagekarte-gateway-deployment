0ff21a61144491a5ff43c6b35baf6949ecbec46b
val8elster <lenchenbears@gmail.com>
2026-08-09 12:26:36 +0200

    modify image name and switch to prod environment

--- scripts/linux/uninstall.sh
+++ scripts/linux/uninstall.sh
@@ -6,7 +6,7 @@ readonly DATA_DIR="${INSTALL_DIR}/data"
 readonly ENV_PATH="${INSTALL_DIR}/.env"
 readonly COMPOSE_PATH="${INSTALL_DIR}/compose.yml"
 
-readonly DEFAULT_IMAGE_REPOSITORY="ghcr.io/val8elster/relais"
+readonly DEFAULT_IMAGE_REPOSITORY="ghcr.io/val8elster/lagekarte-relais:prod"
 
 PURGE=false
 REMOVE_IMAGES=false
